c46f4ca8647c1037f4d173ad2fa27bb2e544e820
cyningsun <cyningsun@gmail.com>
2024-08-12 03:28:00 +0000

    fix #ifndef

--- dbug.py
+++ dbug.py
@@ -70,6 +70,16 @@ def insert_line_after_function(file_content, node, insert_line):
 def process_node(source_file, node, file_content, debug_code):
     changed = False
 
+    # process children first because there are more rearward
+    # Get children sorted by their position, reversed
+    children = list(node.get_children())
+    children.sort(key=lambda x: x.extent.start.line, reverse=True)
+    for child in children:
+        subChanged = False
+        file_content,subChanged = process_node(source_file, child, file_content, debug_code)
+        if subChanged:
+            changed = True
+
     """递归解析AST节点，只打印来自源文件的节点"""
     if node.location.file:
         # 调试输出：打印节点位置和文件名
@@ -84,32 +94,15 @@ def process_node(source_file, node, file_content, debug_code):
                 if subChanged:
                     changed = True
 
-            # Get children sorted by their position, reversed
-            children = list(node.get_children())
-            children.sort(key=lambda x: x.extent.start.line, reverse=True)
-            for child in children:
-                subChanged = False
-                file_content,subChanged = process_node(source_file, child, file_content, debug_code)
-                if subChanged:
-                    changed = True
-    else:
-        # Get children sorted by their position, reversed
-        children = list(node.get_children())
-        children.sort(key=lambda x: x.extent.start.line, reverse=True)
-        for child in children:
-            subChanged = False
-            file_content,subChanged = process_node(source_file, child, file_content, debug_code)
-            if subChanged:
-                changed = True
-
     return file_content, changed
 
 def insert_include_after_includes(file_content, include_line):
     lines = file_content.splitlines()
-    include_indices = [i for i, line in enumerate(lines) if re.match(r'#include\s+[<"].*[>"]', line)]
-    if include_indices:
-        first_include_index = include_indices[0]
-        lines.insert(first_include_index, include_line)
+    pattern = re.compile(r'^\s*(#include|#ifndef|#ifdef)')
+    indices = [i for i, line in enumerate(lines) if pattern.match(line) ]
+    if indices:
+        first_index = indices[0]
+        lines.insert(first_index, include_line)
     else:
         lines.insert(0, include_line)
     return "\n".join(lines)
@@ -145,6 +138,7 @@ def process_files(compile_commands, directory, debug_header, debug_code):
     skip_patterns = ['.*/dbug.h', '.*/dbug.cc', '.*/.*test.cc', '.*/.*test\w.cc']
     file_count = 0
 
+    file_cache = {}
     for entry in compile_commands:
         file_path, args = process_compile_command(entry)
         
@@ -164,6 +158,10 @@ def process_files(compile_commands, directory, debug_header, debug_code):
                     continue
 
                 print(f'Processing file: {file_path}')
+                if file_path in file_cache:
+                    continue
+                file_cache[file_path] = 1
+
                 tu = index.parse(file_path, args)
                 insert_debug_code(tu.cursor, file_path, debug_header, debug_code)
                 file_count += 1
